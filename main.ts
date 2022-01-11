ec9c77bca93a9dc83f494e90d576eb555d53e4d1
bilbobessac <74983423+bilbobessac@users.noreply.github.com>
2022-01-11 18:54:27 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+input.onButtonPressed(Button.A, function () {
+    while (nbre_depart > 0) {
+        basic.showNumber(nbre_depart)
+        nbre_depart += -1
+        basic.pause(1000)
+    }
+    basic.clearScreen()
+    nbre_depart = 10
+})
+let nbre_depart = 0
+basic.clearScreen()
+nbre_depart = 10
